62f1e7319ebcf2ac0f2268eb2c44ea7458db0fe5
Erick Staben <ericklucasstaben@hotmail.com>
2019-10-28 09:35:42 -0300

    no events table seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,7 +18,6 @@ class DatabaseSeeder extends Seeder
             PhaseTableSeeder::class,
             CardTableSeeder::class,
             IntermediarySeeder::class,
-            EventsTableSeeder::class,
          ]);
     }
 }
